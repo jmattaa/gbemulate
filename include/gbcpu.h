5530cdb42ab0184a175063ca3c4963247d3e0097
Jonathan Matta <mattajonathan1@gmail.com>
2025-10-28 14:21:58 +0100

    fix the register ordering to be for example c then b for bc

--- include/gbcpu.h
+++ include/gbcpu.h
@@ -10,8 +10,8 @@ typedef struct
    {
         struct
         {
-            uint8_t a;
             uint8_t f;
+            uint8_t a;
         };
         uint16_t af;
     };
@@ -19,8 +19,8 @@ typedef struct
     {
         struct
         {
-            uint8_t b;
             uint8_t c;
+            uint8_t b;
         };
         uint16_t bc;
     };
@@ -28,8 +28,8 @@ typedef struct
     {
         struct
         {
-            uint8_t d;
             uint8_t e;
+            uint8_t d;
         };
         uint16_t de;
     };
